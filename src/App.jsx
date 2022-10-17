d9c20c428f1c68099f715210f588c44b43b05cd5
Johan van Meeuwen <vanmeeuwen.j@gmail.com>
2022-10-17 14:16:41 +0100

    Basic project card created with props

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from '/src/components/Navbar'
 import Main from '/src/components/Main'
-import Portfolio from '/src/components/Portfolio'
+import ProjectSection from '/src/components/ProjectSection'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <Navbar />
       <Main />
-      <Portfolio />
+      <ProjectSection />
     </div>
   )
 }
