e35f49459e306767a8bc07cc8a39b17af5a6e29b
Johan van Meeuwen <vanmeeuwen.j@gmail.com>
2022-10-17 14:48:23 +0100

    Project card border and shadow

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,21 +3,23 @@ import React from "react"
 export default function Project(props) {
   console.log(props)
   return (
-    <div className="project-card">
-      <div className="date-badge">
-        <p>{props.projectDate}</p>
+    <div className="project-card-shadow">
+      <div className="project-card">
+        <div className="date-badge">
+          <p>{props.projectDate}</p>
+        </div>
+        <img className="project-image" src={props.projectImage} alt="project-image" />
+        <div className="project-info">
+          <h2 className="project-title">{props.projectTitle}</h2>
+          <i className="fa-solid fa-lightbulb"></i>
+          <p>{props.projectIdea}</p>
+          <i className="fa-solid fa-code"></i>
+          <p>{props.projectCode}</p>
+          <i className="fa-solid fa-rocket"></i>
+          <p>{props.projectDuration}</p>
+        </div>
+        <a className="project-button" href={props.projectUrl} target="_">Go to site</a>
       </div>
-      <img className="project-image" src={props.projectImage} alt="project-image" />
-      <div className="project-info">
-        <h2 className="project-title">{props.projectTitle}</h2>
-        <i className="fa-solid fa-lightbulb"></i>
-        <p>{props.projectIdea}</p>
-        <i className="fa-solid fa-code"></i>
-        <p>{props.projectCode}</p>
-        <i className="fa-solid fa-rocket"></i>
-        <p>{props.projectDuration}</p>
-      </div>
-      <a className="project-button" href={props.projectUrl}>Go to site</a>
     </div>
   )
 }
